b802c7d7892134bf377545b1c50559bd7ddca26d
nimchukr <nimchuk2011@gmail.com>
2023-04-19 12:19:40 +0300

    SH:12 - added logger

--- src/modules/bot/middleware/messageController.ts
+++ src/modules/bot/middleware/messageController.ts
@@ -1,7 +1,11 @@
+import { logInfo } from '../../../infrastructure/logger';
 import { isUserKicked } from '../helpers/isUserKicked';
 import type { MyContext } from '../types/main';
 
+
 export const messageController = async (ctx: MyContext, next: () => Promise<void>) => {
+  logInfo(`Message: ${ctx.message}`);
+  logInfo(`Session: ${ctx.session}`);
   if (isUserKicked(ctx)) {
     const id = ctx.message?.from?.id
     if(id) {
